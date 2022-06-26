1941b05fa300afc2c28f2fdad846d2e2edae6fd2
Nineball0528 <timonekakirude@gmail.com>
2022-06-26 17:04:08 +0000

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  
+root to:'public/homes#top'
+
+  scope module: :public do
+    
+  end
+    
+  devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+ get '/admin' => 'admin/homes#top'
+ 
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
